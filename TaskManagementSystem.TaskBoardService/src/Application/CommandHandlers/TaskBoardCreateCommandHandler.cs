e267ff70f4ddbd9b64898b96ae02d4944dfbdf5d
Fakhriddin <fahriddin3040@gmail.com>
2025-07-14 00:09:21 +0500

    Implemented task board create command handler. Next, go to grpc lib to create proto file for endpoint.
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- TaskManagementSystem.TaskBoardService/src/Application/CommandHandlers/TaskBoardCreateCommandHandler.cs
+++ TaskManagementSystem.TaskBoardService/src/Application/CommandHandlers/TaskBoardCreateCommandHandler.cs
@@ -1,4 +1,61 @@
+using MediatR;
+using TaskManagementSystem.SharedLib.Abstractions.Interfaces;
+using TaskManagementSystem.SharedLib.DTO;
+using TaskManagementSystem.SharedLib.Handlers;
+using TaskManagementSystem.SharedLib.Providers.Interfaces;
+using TaskManagementSystem.TaskBoardService.Application.Commands;
+using TaskManagementSystem.TaskBoardService.Application.Results;
+using TaskManagementSystem.TaskBoardService.Core.Aggregates;
+using TaskManagementSystem.TaskBoardService.Core.Interfaces.Policies;
+using TaskManagementSystem.TaskBoardService.Core.Interfaces.Repository;
+
 namespace TaskManagementSystem.TaskBoardService.Application.CommandHandlers;
 
 
-public class TaskBoardCreateCommandHandler : IRequestHand
+public class TaskBoardCreateCommandHandler : IRequestHandler<TaskBoardCreateCommand, Result<TaskBoardCreateResult>>
+{
+    private readonly ExecutionContextDto _executionContext;
+    private readonly ITaskBoardRepository _boardRepository;
+    private readonly IDateTimeService _dateTimeService;
+    private readonly IUniqueTaskBoardNamePolicy _uniqueNamePolicy;
+    private readonly IValidNamePolicy _namePolicy;
+
+    public TaskBoardCreateCommandHandler(
+        IExecutionContextProvider executionContextProvider,
+        IDateTimeService dateTimeService,
+        IUniqueTaskBoardNamePolicy uniqueNamePolicy,
+        ITaskBoardRepository taskBoardRepository,
+        IValidNamePolicy namePolicy)
+    {
+        _executionContext = executionContextProvider.GetContext();
+        _dateTimeService = dateTimeService;
+        _boardRepository = taskBoardRepository;
+        _uniqueNamePolicy = uniqueNamePolicy;
+        _namePolicy = namePolicy;
+    }
+
+    public async Task<Result<TaskBoardCreateResult>> Handle(TaskBoardCreateCommand request, CancellationToken cancellationToken)
+    {
+        var result = await TaskBoardAggregate.Create(
+            name: request.Name,
+            description: request.Description,
+            organizationId: Guid.NewGuid(),
+            createdById: _executionContext.User.Id,
+            cancellationToken: cancellationToken,
+            dateTimeService: _dateTimeService,
+            uniqueNamePolicy: _uniqueNamePolicy,
+            namePolicy: _namePolicy
+        );
+
+        if (result.IsFailure)
+        {
+            return Result<TaskBoardCreateResult>.Failure(result.ErrorDetails);
+        }
+
+        await _boardRepository.CreateAsync(result.Value, cancellationToken);
+
+        return Result<TaskBoardCreateResult>.Success(
+            new (result.Value.Id)
+        );
+    }
+}
--- TaskManagementSystem.TaskBoardService/src/Core/Aggregates/TaskBoardAggregate.cs
+++ TaskManagementSystem.TaskBoardService/src/Core/Aggregates/TaskBoardAggregate.cs
@@ -19,14 +19,13 @@ public sealed class TaskBoardAggregate : TaskBoardModel
 
     public static async Task<Result<TaskBoardAggregate>> Create(
         string name,
-        string description,
+        string? description,
         Guid organizationId,
         Guid createdById,
         CancellationToken cancellationToken,
         IDateTimeService dateTimeService,
         IValidNamePolicy namePolicy,
-        IUniqueTaskBoardNamePolicy uniqueNamePolicy,
-        IOrganizationExistsPolicy organizationExistsPolicy
+        IUniqueTaskBoardNamePolicy uniqueNamePolicy
         )
     {
         var errors = new List<AppExceptionDetail>();
@@ -47,16 +46,6 @@ public sealed class TaskBoardAggregate : TaskBoardModel
                 new(
                     StatusCode: AppExceptionStatusCode.UniqueConstraints,
                     Field: TaskBoardField.Name
-                )
-            );
-        }
-
-        if (! await organizationExistsPolicy.ExistsAsync(organizationId, cancellationToken))
-        {
-            errors.Add(
-                new(
-                    StatusCode: AppExceptionStatusCode.NotFound,
-                    Field: TaskBoardField.Organization.AsForeignKey()
                 )
             );
         }
