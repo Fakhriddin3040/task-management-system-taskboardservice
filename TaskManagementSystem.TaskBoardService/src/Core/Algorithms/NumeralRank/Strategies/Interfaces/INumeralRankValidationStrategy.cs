b8dc2db14094812484341e2f9f3c09aaf35a11fd
Fakhriddin <fahriddin3040@gmail.com>
2025-08-03 20:12:59 +0500

    (ref) Fixed numeral ranks folder structure, removed unused types and fixed tyeps structure and etc.
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

---
+++ TaskManagementSystem.TaskBoardService/src/Core/Algorithms/NumeralRank/Strategies/Interfaces/INumeralRankValidationStrategy.cs
@@ -0,0 +1,8 @@
+namespace TaskManagementSystem.TaskBoardService.Core.Algorithms.NumeralRank.Interfaces;
+
+
+public interface INumeralRankValidationStrategy
+{
+    Task<bool> ValidateAsync(Guid boardId, NumeralRankContext context, CancellationToken cancellationToken);
+    bool CanHandle(NumeralRankContext rankContext);
+}
