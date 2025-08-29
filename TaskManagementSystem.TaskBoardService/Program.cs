17b69d0212e55ec9d776d2966ac4ec710b0dc87c
Fakhriddin <fahriddin3040@gmail.com>
2025-08-29 16:57:09 +0500

    ...
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- TaskManagementSystem.TaskBoardService/Program.cs
+++ TaskManagementSystem.TaskBoardService/Program.cs
@@ -6,12 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationDependencies();
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationGrpc();
+builder.Services.AddGrpcReflection();
 builder.Services.AddNumeralRankAlgorithm();
 builder.Services.AddLogging(l =>
 {
     l.AddConsole();
     l.SetMinimumLevel(LogLevel.Debug);
 });
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: true).AddEnvironmentVariables();
 
 var app = builder.Build();
 
